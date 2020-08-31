cf2f3e2d38c91b395c49f2ffbe4b4fc23ceaaf40
Julian <julian@jgroen.com>
2020-08-31 01:01:20 -0600

    passing file up to App.js via onChange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FileUploader from './components/FileUploader';
 
 function App() {
+    const [file, setFile] = useState(null);
+    const changeHandler = (newFile) => {
+        setFile(newFile);
+    };
+
     return (
         <div className="bg-indigo-100 fixed w-full h-full overflow-auto">
-            <FileUploader />
+            {file && file.type}
+            <FileUploader onChange={changeHandler} />
         </div>
     );
 }
--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FileUploader = () => {
+const FileUploader = ({ onChange }) => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        if (selected && types.includes(selected.type)) {
+            onChange(selected);
+            setFile(selected);
+            setError(null);
+        } else {
+            setFile(null);
+            setError('Please select an image (PNG or JPG/JPEG)');
+        }
+    };
+
     return (
         <div>
             <form>
-                <input type="file" />
+                <input type="file" onChange={changeHandler} />
+                {error && <div>{error}</div>}
+                {file && <div>{file.name}</div>}
             </form>
         </div>
     );
