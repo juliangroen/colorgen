8d444f957d9a6cee87235c8ece4fb728c399d3f3
Julian <julian@jgroen.com>
2020-08-31 23:58:58 -0600

    moved generateColors logic to a single function

--- src/components/ColorScheme.js
+++ src/components/ColorScheme.js
@@ -3,41 +3,34 @@ import React, { useState, useEffect } from 'react';
 const ColorScheme = ({ file }) => {
     const [colors, setColors] = useState(null);
 
-    const combinePixels = (pixels) => {
-        const combined = pixels.map((pixel) => {
-            const rgbArray = pixel.split(',');
-            rgbArray.pop();
-            const hexArray = rgbArray.map((value) => {
-                const hex = parseInt(value).toString(16);
-                return hex.length === 1 ? `0${hex}` : hex;
-            });
-            return ['#', ...hexArray].join('');
-        });
-        return combined;
-    };
-
-    const generateColors = (file) => {
+    const generateColors = (image) => {
         const canvas = document.querySelector('canvas');
         const ctx = canvas.getContext('2d');
         const img = document.createElement('img');
-        img.src = window.URL.createObjectURL(file);
+        img.src = window.URL.createObjectURL(image);
         img.onload = () => {
             canvas.width = 32;
             canvas.height = 32;
             ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
             const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-            const pixels = [];
             let holder = [];
+            const pixelColors = [];
             imageData.map((value, index) => {
                 if ((index + 1) % 4 === 0) {
                     holder.push(value);
-                    pixels.push(holder.join(','));
+                    const hexArray = holder.map((value) => {
+                        const hex = parseInt(value).toString(16);
+                        return hex.length === 1 ? `0${hex}` : hex;
+                    });
+                    hexArray.pop();
+                    pixelColors.push(['#', ...hexArray].join(''));
+                    //pixels.push(holder.join(','));
                     holder = [];
                 } else {
                     holder.push(value);
                 }
             });
-            setColors(combinePixels(pixels));
+            setColors(pixelColors);
         };
     };
 
@@ -48,7 +41,7 @@ const ColorScheme = ({ file }) => {
     return (
         <div>
             <canvas>HTML5 canvas not supported in your browser.</canvas>
-            <div className="flex flex-wrap max-w-xl">
+            <div className="flex flex-wrap max-w-lg">
                 {colors &&
                     colors.map((color, index) => {
                         return <div key={index} className="w-4 h-4" style={{ background: color }}></div>;
