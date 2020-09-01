538684e8471e706c518ef3303b226b9a87127dd3
Julian <julian@jgroen.com>
2020-08-31 18:07:18 -0600

    outputting pixels as comma separated

--- src/components/ColorScheme.js
+++ src/components/ColorScheme.js
@@ -1,28 +1,31 @@
 import React, { useEffect } from 'react';
 
 const ColorScheme = ({ file }) => {
+    const combinePixels = (pixels) => {
+        const combined = [];
+        return combined;
+    };
     const generateColors = (file) => {
         const canvas = document.querySelector('canvas');
         const ctx = canvas.getContext('2d');
         const img = document.createElement('img');
         img.src = window.URL.createObjectURL(file);
         img.onload = () => {
-            ctx.canvas.width = img.width;
-            ctx.canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
+            canvas.width = 32;
+            canvas.height = 32;
+            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
             const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
             const pixels = [];
             let holder = [];
             imageData.map((value, index) => {
                 if ((index + 1) % 4 === 0) {
                     holder.push(value);
-                    pixels.push(holder);
+                    pixels.push(holder.join(','));
                     holder = [];
                 } else {
                     holder.push(value);
                 }
             });
-            pixels.map();
             console.log(pixels);
         };
     };
